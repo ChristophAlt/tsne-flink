4d469b3d1d16673e027c5a094c52664b0b452f4e
Christoph Alt <christoph.alt@posteo.de>
2015-07-03 12:56:39 +0200

    Use SparseVectors as input

--- src/test/scala/TsneHelpersTestSuite.scala
+++ src/test/scala/TsneHelpersTestSuite.scala
@@ -18,13 +18,12 @@
 
 package de.tu_berlin.dima.impro3
 
+import de.tu_berlin.dima.impro3.TsneHelpers._
 import org.apache.flink.api.scala._
 import org.apache.flink.ml.common.LabeledVector
-import org.apache.flink.ml.math.DenseVector
+import org.apache.flink.ml.math.SparseVector
 import org.apache.flink.ml.metrics.distances.SquaredEuclideanDistanceMetric
 import org.scalatest._
-
-import de.tu_berlin.dima.impro3.TsneHelpers._
 
 
 class TsneHelpersTestSuite extends FlatSpec with Matchers with Inspectors {
@@ -34,7 +33,7 @@ class TsneHelpersTestSuite extends FlatSpec with Matchers with Inspectors {
     val neighbors = 2
     val metric = SquaredEuclideanDistanceMetric()
 
-    val input = env.fromCollection(TsneHelpersTestSuite.input)
+    val input = env.fromCollection(TsneHelpersTestSuite.inputDense)
 
     val results = kNearestNeighbors(input, neighbors, metric).collect()
     val expectedResults = TsneHelpersTestSuite.knnResults
@@ -50,7 +49,7 @@ class TsneHelpersTestSuite extends FlatSpec with Matchers with Inspectors {
     val neighbors = 10
     val metric = SquaredEuclideanDistanceMetric()
 
-    val input = env.fromCollection(TsneHelpersTestSuite.input)
+    val input = env.fromCollection(TsneHelpersTestSuite.inputDense)
 
     val knn = kNearestNeighbors(input, neighbors, metric)
     val results = pairwiseAffinities(knn, perplexity).collect()
@@ -88,16 +87,16 @@ class TsneHelpersTestSuite extends FlatSpec with Matchers with Inspectors {
 }
 
 object TsneHelpersTestSuite {
-  val input: Seq[LabeledVector] = List(
-    LabeledVector(0.0, DenseVector(0.0, 0.0, 0.0, 0.0)),
-    LabeledVector(1.0, DenseVector(1.0, 1.0, 1.0, 1.0)),
-    LabeledVector(2.0, DenseVector(2.0, 2.0, 2.0, 2.0)),
-    LabeledVector(3.0, DenseVector(3.0, 3.0, 3.0, 3.0)),
-    LabeledVector(4.0, DenseVector(4.0, 4.0, 4.0, 4.0)),
-    LabeledVector(5.0, DenseVector(5.0, 5.0, 5.0, 5.0)),
-    LabeledVector(6.0, DenseVector(6.0, 6.0, 6.0, 6.0)),
-    LabeledVector(7.0, DenseVector(7.0, 7.0, 7.0, 7.0)),
-    LabeledVector(8.0, DenseVector(8.0, 8.0, 8.0, 8.0))
+  val inputDense: Seq[LabeledVector] = List(
+    LabeledVector(0.0, SparseVector.fromCOO(4, List((0, 0.0),(1, 0.0),(2, 0.0),(3, 0.0)))),
+    LabeledVector(1.0, SparseVector.fromCOO(4, List((0, 1.0),(1, 1.0),(2, 1.0),(3, 1.0)))),
+    LabeledVector(2.0, SparseVector.fromCOO(4, List((0, 2.0),(1, 2.0),(2, 2.0),(3, 2.0)))),
+    LabeledVector(3.0, SparseVector.fromCOO(4, List((0, 3.0),(1, 3.0),(2, 3.0),(3, 3.0)))),
+    LabeledVector(4.0, SparseVector.fromCOO(4, List((0, 4.0),(1, 4.0),(2, 4.0),(3, 4.0)))),
+    LabeledVector(5.0, SparseVector.fromCOO(4, List((0, 5.0),(1, 5.0),(2, 5.0),(3, 5.0)))),
+    LabeledVector(6.0, SparseVector.fromCOO(4, List((0, 6.0),(1, 6.0),(2, 6.0),(3, 6.0)))),
+    LabeledVector(7.0, SparseVector.fromCOO(4, List((0, 7.0),(1, 7.0),(2, 7.0),(3, 7.0)))),
+    LabeledVector(8.0, SparseVector.fromCOO(4, List((0, 8.0),(1, 8.0),(2, 8.0),(3, 8.0))))
   ).toSeq
 
   // result for k = 2
